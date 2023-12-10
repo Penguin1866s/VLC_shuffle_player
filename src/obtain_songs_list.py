931e68cbeac69a904ebf0ba00d27095a6b929d4a
Penguin1866 <anonymous@example.com>
2023-12-10 22:29:40 +0100

    docs: Remove explanatory comment
    Remove the explanatory comment in "obtain_songs_list.py" archive.

--- src/obtain_songs_list.py
+++ src/obtain_songs_list.py
@@ -6,14 +6,3 @@ def obtain_songs_list(root):
         location = track.find("ns:location", namespace).text
         songs.append({"title": title, "location": location})
     return songs
-
-#(for better comprension of how it works and how is the use of the variable "namespace").
-###Is equivalent to
-#-->
-#def obtain_songs_list(root):
-#    songs = []
-#    for track in root.findall(".//{http://xspf.org/ns/0/}trackList/{http://xspf.org/ns/0/}track"):
-#        title = track.find("{http://xspf.org/ns/0/}title").text
-#        location = track.find("{http://xspf.org/ns/0/}location").text
-#        songs.append({"title": title, "location": location})
-#    return songs
