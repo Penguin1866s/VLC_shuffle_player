6ef414db4ca8e76dd58e47c2da834cd03a4a5857
Penguin1866 <anonymous@example.com>
2023-12-11 01:45:05 +0100

    refactor: Changes due implementation of randomizer_list feature

--- VLC_shuffle_player.py
+++ VLC_shuffle_player.py
@@ -1,9 +1,11 @@
 from src.read_archive_xspf import read_archive_xspf
 from src.obtain_songs_list import obtain_songs_list
+from src.randomizer_list import randomizer_list
 from src.play_songs import play_songs
 
 if __name__ == "__main__":
     path_arvhive_xspf = "list_songs.xspf"
     root = read_archive_xspf(path_arvhive_xspf)
     list_songs = obtain_songs_list(root)
-    play_songs(list_songs)
+    randomized_list = randomizer_list(list_songs)
+    play_songs(randomized_list)
